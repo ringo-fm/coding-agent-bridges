0c79fbf1d10c1bb50619b359b747bf2be384c186
f12o <14793610+f4ah6o@users.noreply.github.com>
2026-06-19 11:20:33 +0900

    Add Codex adapter boundary

---
+++ Sources/CodexAdapter/CodexAdapter.swift
@@ -0,0 +1,8 @@
+import AFMBackend
+import AgentBridgeCore
+import BridgeHTTP
+
+public enum CodexAdapter {
+    public static let defaultModel = "apple-foundation-local"
+    public static let defaultPort = 8765
+}
